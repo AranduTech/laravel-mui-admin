07a1fa7b1ea45e58ff651aeb75c3126ac4377192
Bruno Polo <brunopolo@poli.ufrj.br>
2023-11-17 16:39:03 -0300

    Refactor app initialization in admin.js and guest.js

--- stubs/js/admin.js
+++ stubs/js/admin.js
@@ -1,11 +1,9 @@
-import { bootstrapApp, app } from '@arandu/laravel-mui-admin';
+import { app } from '@arandu/laravel-mui-admin';
 
 import createApp from './src/app';
 import config from './src/config';
 
-bootstrapApp(config);
-
-app.init().then(() => {
+app.init(config).then(() => {
     createApp('admin');
 });
 
--- stubs/js/guest.js
+++ stubs/js/guest.js
@@ -1,10 +1,8 @@
-import { bootstrapApp, app } from '@arandu/laravel-mui-admin';
+import { app } from '@arandu/laravel-mui-admin';
 
 import createApp from './src/app';
 import config from './src/config';
 
-bootstrapApp(config);
-
-app.init().then(() => {
+app.init(config).then(() => {
     createApp('guest');
 });
