d1f6e9d9d2fd2c63c85ce74e46acacc9cd96372f
GabrielARamosDev <gabrihot97@hotmail.com>
2024-01-16 20:14:29 -0300

    Update Content-Disposition header in DashboardController.php

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -111,7 +111,7 @@ class DashboardController extends Controller
 
         // Prepare headers
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="'. $filename .'"');
+        header('Content-Disposition: attachment; filename="'. $filename .'.xlsx"');
         header('Cache-Control: max-age=0');
 
         $writer = new Xlsx($spreadsheet);
