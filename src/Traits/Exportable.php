13831f81ee31284755cecfb6a6d229cfaf2f7f31
GabrielARamosDev <gabrihot97@hotmail.com>
2024-02-22 18:25:51 -0300

    Refactor getExportsHeadings method in Exportable trait

--- src/Traits/Exportable.php
+++ src/Traits/Exportable.php
@@ -5,10 +5,15 @@ namespace Arandu\LaravelMuiAdmin\Traits;
 trait Exportable
 {
 
-    public static function getExportsHeadings()
-    {
+    public static function getExportsHeadings(
+        $removeColumns = [],
+        $extraColumns = [],
+        $translatedColumns = null
+    ): array {
+
         /** @var \Illuminate\Database\Eloquent\Model */
         $instance = new static;
+        
         $columns = $instance->getFillable();
 
         if (!in_array($instance->getKeyName(), $columns)) {
@@ -24,6 +29,22 @@ trait Exportable
                 $instance->getCreatedAtColumn(),
                 $instance->getUpdatedAtColumn(),
             ];
+        }
+
+        $columns = array_merge(
+            $columns,
+            $extraColumns
+        );
+
+        $columns = array_diff(
+            $columns, 
+            $removeColumns
+        );
+
+        if ($translatedColumns) {
+            $columns = array_map(function ($column) use ($translatedColumns) {
+                return $translatedColumns[$column] ?? $column;
+            }, $columns);
         }
 
         return $columns;
